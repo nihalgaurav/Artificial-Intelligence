0c669d7bc8e0891de10bb618fae78d1e7fdbcf16
nihalgaurav <nihalgaurav85@gmail.com>
2019-07-06 20:32:06 +0530

    train_test_split

---
+++ ai_package01/AI_Practical57.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+import warnings
+warnings.filterwarnings("ignore")
+
+filename = 'indians-diabetes.data.csv'
+
+hnames = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pd.read_csv(filename, names= hnames)
+array = dataframe.values
+
+x = array[:, 0:8]
+y = array[:, 8]
+
+test_size = 0.33
+seed = 7
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size,
+                                                    random_state=seed)
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+predicted = model.predict(x_test)
+results = classification_report(y_test, predicted)
+
+print(results)
+print()
